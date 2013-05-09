3cc6a22c7749bf3f81fabe0df383dd67245be776
Mikey Fujihara <mikeyfujihara@apple.com>
2013-05-09 10:47:47 -0700

    chapter 6 no challenges

---
+++ Hyponosister/Hyponosister/HypnosisView.m
@@ -0,0 +1,84 @@
+//
+//  HypnosisView.m
+//  Hyponosister
+//
+//  Created by Mikey Fujihara on 5/9/13.
+//  Copyright (c) 2013 Mikey Fujihara. All rights reserved.
+//
+
+#import "HypnosisView.h"
+
+@implementation HypnosisView
+@synthesize circleColor;
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setBackgroundColor:[UIColor clearColor]];
+        [self setCircleColor:[UIColor lightGrayColor]];
+    }
+    return self;
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGRect bounds = [self bounds];
+    
+    CGPoint center;
+    center.x = bounds.origin.x + bounds.size.width / 2.0;
+    center.y = bounds.origin.y + bounds.size.height / 2.0;
+    
+    float maxRadius = hypot(bounds.size.width, bounds.size.height) / 2.0;
+    
+    CGContextSetLineWidth(ctx, 10);
+    
+    CGContextSetRGBStrokeColor(ctx, 0.6, 0.6, 0.6, 1.0);
+    [circleColor setStroke];
+    
+    for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
+        CGContextAddArc(ctx, center.x, center.y, currentRadius, 0, M_PI * 2.0, YES);
+        
+        CGContextStrokePath(ctx);
+    }
+    
+    NSString *text = @"You are getting sleepy";
+    
+    UIFont *font = [UIFont boldSystemFontOfSize:28];
+    CGRect textRect;
+    
+    textRect.size = [text sizeWithFont:font];
+    textRect.origin.x = center.x - textRect.size.width / 2.0;
+    textRect.origin.y = center.y - textRect.size.height / 2.0;
+    
+    [[UIColor blackColor] setFill];
+    
+    CGSize offset = CGSizeMake(4, 3);
+    
+    CGColorRef color = [[UIColor darkGrayColor] CGColor];
+    
+    CGContextSetShadowWithColor(ctx, offset, 2.0, color);
+    
+    [text drawInRect:textRect withFont:font];
+}
+
+- (BOOL)canBecomeFirstResponder
+{
+    return YES;
+}
+
+- (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    NSLog(@"Device started shaking");
+    if (motion == UIEventSubtypeMotionShake) {
+        [self setCircleColor:[UIColor redColor]];
+    }
+}
+- (void)setCircleColor:(UIColor *)clr
+{
+    circleColor = clr;
+    [self setNeedsDisplay];
+}
+
+@end
---
+++ Hyponosister/Hyponosister/HypnosisterAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hyponosister
+//
+//  Created by Mikey Fujihara on 5/9/13.
+//  Copyright (c) 2013 Mikey Fujihara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HypnosisView.h"
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate, UIScrollViewDelegate>
+{
+    HypnosisView *view;
+}
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
